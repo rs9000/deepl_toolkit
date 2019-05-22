b9ec97fd76ead1e96ef271fd7f3fa71081baa8ea
Rs <rs.dicarlo@gmail.com>
2019-05-22 14:37:34 +0200

    Add files via upload

--- deepl_toolkit/layers.py
+++ deepl_toolkit/layers.py
@@ -1,87 +1,119 @@
 import torch
 from torch import nn
 import numpy as np
+import torch.nn.init as init
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-class bi_lstm_encoder(nn.Module):
+class attention_layer(nn.Module):
     """
-    LSTM encoder
-    Embedding(question) -> RNN()
+    Attention Layer
 
     """
 
-    def __init__(self, embedding_layer, rnn_dim, embedding_size=100, rnn_num_layers=2, rnn_dropout=0.4):
-        super(bi_lstm_encoder, self).__init__()
+    def __init__(self, input_dim, hidden_dim):
+        super(attention_layer, self).__init__()
+        self.Wv = nn.Conv2d(input_dim, hidden_dim, kernel_size=1, padding=0)
+        self.Wu = nn.Linear(input_dim, hidden_dim)
+        self.Wp = nn.Conv2d(hidden_dim, 1, kernel_size=1, padding=0)
 
-        self.NULL = 1
-        self.START = 0
-        self.END = 2
+        self.hidden_dim = hidden_dim
+        self.attention_maps = None
 
-        self.embed = embedding_layer
-        self.rnn_size = rnn_dim
-        self.rnn = nn.LSTM(embedding_size, rnn_dim, rnn_num_layers,
-                           dropout=rnn_dropout, batch_first=True, bidirectional=True).to(device)
-
-        self.init_weights()
-
-    def init_weights(self):
+    def getMap(self):
         """
-        Initialize weights in RNN
+        Get saved attention map
+
+        return: Attention map
 
         """
 
-        for name, param in self.rnn.named_parameters():
-            if 'weight' in name:
-                torch.nn.init.xavier_uniform_(param.data)
-            if 'bias' in name:
-                param.data.fill_(0)
+        return torch.squeeze(self.attention_maps[0], 1)
 
-    def forward(self, x):
-        # type: (torch.Tensor) -> (torch.Tensor, torch.Tensor)
+    def forward(self, v, u):
+        # type: (torch.Tensor, torch.Tensor) -> torch.Tensor
         """
         Forward Pass
 
-        x: question batch_size x question_len
-
-        return: hs = question (batch_size X question_len X rnn_dim)
-                idx = question-length, without padding (batch_size x 1)
+        v: visual features (N x D x H x W)
+        u: attention key (N x D)
+        return: attention map (N x D)
 
         """
 
-        N, T = x.shape
-        idx = torch.LongTensor(N).fill_(T - 1)
+        N, K = v.size(0), self.hidden_dim
+        D, H, W = v.size(1), v.size(2), v.size(3)
+        v_proj = self.Wv(v)  # N x K x H x W
+        u_proj = self.Wu(u)  # N x K
+        u_proj_expand = u_proj.view(N, K, 1, 1).expand(N, K, H, W)
+        h = F.tanh(v_proj + u_proj_expand)
+        h = self.Wp(h).view(N, H * W)
+        p = F.softmax(h, -1).view(N, 1, H, W)
+        self.attention_maps = p.data.clone()
 
-        # Find the last non-null element in each sequence
-        x_cpu = x.data.cpu()
-        for i in range(N):
-            for t in range(T - 1):
-                if x_cpu[i, t] != self.NULL and x_cpu[i, t + 1] == self.NULL:
-                    idx[i] = t
-                    break
-        idx = idx.type_as(x.data).long()
-        idx.requires_grad = False
+        v_tilde = (p.expand_as(v) * v).sum(3).sum(2).view(N, D)
+        return v_tilde
 
-        hs, _ = self.rnn(self.embed(x))
 
-        hs = hs.view(N, T, self.rnn_size, 2)
 
-        # Split in forward and backward sequence
-        output_forward, output_backward = torch.chunk(hs, 2, 3)
-        output_forward = output_forward.squeeze(3)  # (batch_size, T, hidden_size)
-        output_backward = output_backward.squeeze(3)  # (batch_size, T, hidden_size)
+class seq2seq_rnn(nn.Module):
+    def __init__(self, input_features, rnn_features, num_layers=1, drop=0.0,
+               rnn_type='LSTM', rnn_bidirectional=False):
+        super(seq2seq_rnn, self).__init__()
+        self.bidirectional = rnn_bidirectional
 
-        # Find last elements of the forward sequence
-        q_len = idx.view(N, 1, 1).expand(N, 1, self.rnn_size).to(device)
+        if rnn_type == 'LSTM':
+          self.rnn = nn.LSTM(input_size=input_features,
+                    hidden_size=rnn_features, dropout=drop,
+                    num_layers=num_layers, batch_first=True,
+                    bidirectional=rnn_bidirectional)
+        elif rnn_type == 'GRU':
+          self.rnn = nn.GRU(input_size=input_features,
+                    hidden_size=rnn_features, dropout=drop,
+                    num_layers=num_layers, batch_first=True,
+                    bidirectional=rnn_bidirectional)
+        else:
+          raise ValueError('Unsupported Type')
 
-        # Trunk the forward sequence at t = question_len
-        output_forward = output_forward.gather(1, q_len).view(N, self.rnn_size)
-        # Get last state of the backward sequence t = 0
-        output_backward = output_backward[:, 0, :]
-        # Re-concat output
-        output = torch.cat((output_forward, output_backward), -1)
-        return output
+        self.init_weight(rnn_bidirectional, rnn_type)
+
+    def init_weight(self, bidirectional, rnn_type):
+        self._init_rnn(self.rnn.weight_ih_l0, rnn_type)
+        self._init_rnn(self.rnn.weight_hh_l0, rnn_type)
+        self.rnn.bias_ih_l0.data.zero_()
+        self.rnn.bias_hh_l0.data.zero_()
+
+        if bidirectional:
+          self._init_rnn(self.rnn.weight_ih_l0_reverse, rnn_type)
+          self._init_rnn(self.rnn.weight_hh_l0_reverse, rnn_type)
+          self.rnn.bias_ih_l0_reverse.data.zero_()
+          self.rnn.bias_hh_l0_reverse.data.zero_()
+
+    def _init_rnn(self, weight, rnn_type):
+        chunk_size = 4 if rnn_type == 'LSTM' else 3
+        for w in weight.chunk(chunk_size, 0):
+            init.xavier_uniform(w)
+
+    def forward(self, q_emb, lengths):
+        lens, indices = torch.sort(lengths, 0, True)
+
+        packed = pack_padded_sequence(q_emb[indices.to(device)], lens.tolist(), batch_first=True)
+        if isinstance(self.rnn, nn.LSTM):
+            _, ( outputs, _ ) = self.rnn(packed)
+        elif isinstance(self.rnn, nn.GRU):
+            _, outputs = self.rnn(packed)
+
+        if self.bidirectional:
+            outputs = torch.cat([ outputs[0, :, :], outputs[1, :, :] ], dim=1)
+        else:
+            outputs = outputs.squeeze(0)
+
+        _, _indices = torch.sort(indices, 0)
+        outputs = outputs[_indices.to(device)]
+
+        return outputs
 
 
 class glove_embedding(nn.Module):
@@ -145,16 +177,17 @@ class contrastive_loss(nn.Module):
 
     """
 
-    def __init__(self, margin):
+    def __init__(self, margin, temperature=1):
         super(contrastive_loss, self).__init__()
         self.margin = margin
         self.eps = 1e-9
-        self.criterion = nn.MSELoss(reduction='none').cuda()
+        self.criterion = nn.MSELoss(reduction='none').to(device)
+        self.temperature = temperature
 
-    def forward(self, output1, output2, target, magnitude=1):
+    def forward(self, output1, output2, target):
 
         euclidean_distance = self.criterion(output1, output2).mean(1)
         loss_contrastive = torch.mean((1 - target) * euclidean_distance +
-                                      (target) * torch.clamp(self.margin - euclidean_distance, min=0.0)).mul(magnitude)
+                                      (target) * torch.clamp(self.margin - euclidean_distance, min=0.0))
 
-        return loss_contrastive
+        return loss_contrastive / self.temperature
--- deepl_toolkit/utils.py
+++ deepl_toolkit/utils.py
@@ -5,6 +5,22 @@ import numpy as np
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+
+def get_tensor_lengths(x, null_char):
+    N, T = x.shape
+    idx = torch.LongTensor(N).fill_(T - 1)
+
+    # Find the last non-null element in each sequence
+    x_cpu = x.data.cpu()
+    for i in range(N):
+        for t in range(T - 1):
+            if x_cpu[i, t] != null_char and x_cpu[i, t + 1] == null_char:
+                idx[i] = t
+                break
+    idx = idx.type_as(x.data).long()
+    idx.requires_grad = False
+    return idx
+
 def load_json(filename):
     with open(filename) as f:
         vocab = json.load(f)
@@ -15,3 +31,35 @@ def save_json(filename, data):
     with open(filename, "w") as text_file:
         text_file.write(json.dumps(data))
     return
+
+
+def restrict_w2v(w2v, restricted_word_set, filename):
+    new_vectors = []
+    new_vocab = {}
+    new_index2entity = []
+    new_vectors_norm = []
+
+    for i in range(len(w2v.vocab)):
+        word = w2v.index2entity[i]
+        vec = w2v.vectors[i]
+        vocab = w2v.vocab[word]
+        new_vectors_norm = None
+        if word in restricted_word_set:
+            vocab.index = len(new_index2entity)
+            new_index2entity.append(word)
+            new_vocab[word] = vocab
+            new_vectors.append(vec)
+
+    w2v.vocab = new_vocab
+    w2v.vectors = np.array(new_vectors)
+    w2v.index2entity = new_index2entity
+    w2v.index2word = new_index2entity
+    w2v.vectors_norm = new_vectors_norm
+    w2v.init_sims()
+    w2v.save_word2vec_format(filename)
+
+def update_learning_rate(optimizer, epoch, init_lr=0.001, decay=15):
+    learning_rate =  init_lr * 0.5**(float(epoch) / decay)
+    for param_group in optimizer.param_groups: param_group['lr'] = learning_rate
+
+    return learning_rate
